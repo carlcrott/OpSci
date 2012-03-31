563a62c9361253104c82a000c07584c3fb1225a8
“Carl <carlcrott@gmail.com>
2012-03-30 20:43:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Skraper::Application.config.secret_token = 'b60e678ae11a3caaf52d93651d51be46eaf6f7cf46ea9486b799793cb54865e822044f5d213b76dcf36c27baf5ea2c5e5528f77feea07c15690ad4e74ad4cf50'
